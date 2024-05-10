a958ca409fd3efcae6832588d3df7068bcb09d93
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2024-05-10 14:06:42 +0200

    upd bool

---
+++ bool.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h> 
+int main(){
+    // Create boolean variables
+    bool isProgrammingFun = true;
+    bool isFishTasty = false;
+
+    // Return boolean values
+    printf("%d", isProgrammingFun);   // Returns 1 (true)
+    printf("%s","\n");
+    printf("%d", isFishTasty);        // Returns 0 (false) 
+    printf("%s","\n");
+    return 0;
+}
+
